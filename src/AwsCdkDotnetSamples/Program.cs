97d25246d97658b5bbb0f57a53a33963802484fd
Ankush Jain <ankushjain358@gmail.com>
2022-12-28 18:45:09 +0530

    Initial commit

---
+++ src/AwsCdkDotnetSamples/Program.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AwsCdkDotnetSamples
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            
+            // Uncomment this to deploy static site resources including S3 & CloudFront
+            //new StaticSiteWithS3AndCloudFrontStack(app, "StaticSite_Stack");
+           
+            
+            app.Synth();
+        }
+    }
+}
---
+++ src/AwsCdkDotnetSamples/static-site-s3-cloudfront/StaticSiteWithS3AndCloudFrontStack.cs
@@ -0,0 +1,46 @@
+﻿using Amazon.CDK;
+using Amazon.CDK.AWS.CloudFront;
+using Amazon.CDK.AWS.S3;
+using Amazon.CDK.AWS.S3.Deployment;
+using Constructs;
+using System.Net.Sockets;
+
+namespace AwsCdkDotnetSamples
+{
+    internal class StaticSiteWithS3AndCloudFrontStack : Stack
+    {
+        internal StaticSiteWithS3AndCloudFrontStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // 1. Create S3 bucket
+            var siteBucket = new Bucket(this, "Bucket", new BucketProps
+            {
+                BucketName = id,
+                Versioned = false,
+                PublicReadAccess = true,
+            });
+
+            // 2. Creating the CloudFront distribution that serves the files from the S3 bucket
+            var behavior = new Behavior();
+            behavior.IsDefaultBehavior = true;
+
+            var originAccessIdentity = new OriginAccessIdentity(this, "OAI");
+            siteBucket.GrantRead(originAccessIdentity);
+
+            var distribution = new CloudFrontWebDistribution(this, "WebDistribution", new CloudFrontWebDistributionProps
+            {
+                OriginConfigs = new ISourceConfiguration[]
+               {
+                    new SourceConfiguration
+                    {
+                        S3OriginSource = new S3OriginConfig
+                        {
+                            S3BucketSource = siteBucket,
+                            OriginAccessIdentity = originAccessIdentity
+                        },
+                        Behaviors = new Behavior[] { behavior }
+                    }
+               }
+            });
+        }
+    }
+}
